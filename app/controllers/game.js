93ea8664225bcf6550225d7920bfbcbdd591b044
Creadak <creadak@gmail.com>
2015-02-22 18:04:49 +0000

    Actually fix undefined reference #11

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -423,7 +423,7 @@ var Game = function Game(channel, client, config, cmdArgs) {
                         playerCards.removeCard(card);
                     });
 
-                    self.discard.answer.push(playerCards);
+                    self.discards.answer.push(playerCards);
                     player.hasDiscarded = true;
                     player.points -= 1;
 
